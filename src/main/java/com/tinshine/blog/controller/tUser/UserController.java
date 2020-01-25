44b141ad6a6acb844c02c414b3cc8ba20d7a1eed
T-ian <3553407189@qq.com>
2020-01-25 18:06:55 +0800

    nothing special changed

--- src/main/java/com/tinshine/blog/controller/tUser/UserController.java
+++ src/main/java/com/tinshine/blog/controller/tUser/UserController.java
@@ -2,6 +2,7 @@ package com.tinshine.blog.controller.tUser;
 
 import com.tinshine.blog.service.tUser.UserServiceImpl;
 import com.tinshine.blog.entity.UserEntity;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    private Logger logger = Logger.getLogger(this.getClass());
 
     @Autowired
     private UserServiceImpl userService;
@@ -30,8 +33,11 @@ public class UserController {
     @RequestMapping("login.json") // 处理登录界面表单提交过来的数据
     @ResponseBody
     public String login (Model map, HttpServletRequest request) {
+        logger.debug("开始登录");
         String name = request.getParameter("userName");
+        logger.info("用户名为：" + name);
         String pswd = request.getParameter("password");
+        logger.info("密码为：" + pswd);
 
         UserEntity userFound = userService.searchUser(name, pswd);
         if (userFound == null)
